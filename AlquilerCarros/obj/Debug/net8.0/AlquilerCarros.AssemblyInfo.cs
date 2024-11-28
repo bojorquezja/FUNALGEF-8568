43975394cfc1c15f043a8290148c50af0cb0faba
Sr_L3M0N <italocarcheri@gmail.com>
2024-11-28 09:31:58 -0500

    4to commit

--- AlquilerCarros/obj/Debug/net8.0/AlquilerCarros.AssemblyInfo.cs
+++ AlquilerCarros/obj/Debug/net8.0/AlquilerCarros.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlquilerCarros")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be5bcc520162551856fa750538e63e3fbdac3f27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3767666b85753181e6b2811870bfa49b350ddaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlquilerCarros")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlquilerCarros")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
