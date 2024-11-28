3a8cd19da71d9f8b72ecb1dd3c8e39a33e731f84
ELNICOXPROGRAMER <lopezsaavedranicolas6@gmail.com>
2024-11-28 09:13:59 -0500

    Primera

--- AlquilerCarros/AlquilerUtil.cs
+++ AlquilerCarros/AlquilerUtil.cs
@@ -8,21 +8,20 @@ namespace AlquilerCarros
 {
     internal class AlquilerUtil
     {
-        
-        
+
+
         public static void alquilerutil()
         {
             Alquiler alquiler = new Alquiler();
             Console.WriteLine("Ingrese el numero de placa:");
-            alquiler.Numero=int.Parse(Console.ReadLine());
+            alquiler.Numero = int.Parse(Console.ReadLine());
             Console.WriteLine("Ingrese el carro:");
             alquiler.Carro = Console.ReadLine();
             Console.WriteLine("ingrese el contratante");
             alquiler.Cliente = Console.ReadLine();
             Console.WriteLine("Ingrese el precio:");
-            alquiler.Precio=double.Parse(Console.ReadLine());   
-
+            alquiler.Precio = double.Parse(Console.ReadLine());
         }
-
     }
 }
+
