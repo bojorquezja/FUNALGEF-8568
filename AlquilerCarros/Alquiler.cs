7c58a75d5da9f011e6688ecf92cb6d84ee7d66eb
Fabri-Programmer <fabrizziopks@hotmail.com>
2024-11-28 08:07:00 -0500

    PrimerGuardado

---
+++ AlquilerCarros/Alquiler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AlquilerCarros
+{
+    internal class Alquiler
+    {
+        public int Numero { get; set; }
+        public string Carro { get; set; }
+        public string Cliente { get; set; }
+        public double Precio { get; set; }
+
+        public override string ToString()
+        {
+            return "Numero:" + Numero + "Carro: " + Carro + "Cliente: " + Cliente + "Precio: " + Precio;
+        }
+
+    }
+}
---
+++ AlquilerCarros/AlquilerUtil.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlquilerCarros
+{
+    internal class AlquilerUtil
+    {
+        
+        
+        public static void alquilerutil()
+        {
+
+        }
+
+    }
+}
