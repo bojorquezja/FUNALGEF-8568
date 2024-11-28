0ef8d3bfdf971c8eb0b41e75dda26fb86c5de19e
ANDRE04PROGRAMER <andre.alarcon.2004@gmail.com>
2024-11-28 10:06:23 -0500

    Terminado si

--- AlquilerCarros/Program.cs
+++ AlquilerCarros/Program.cs
@@ -28,6 +28,8 @@ int[]arr = new int[2];
 int menu_number, menu_number_carro, array_length = 0, array2_length = 0, elim = 0;
 string[] desayuno = new string[array_length];
 CarroUtil carro_u = new CarroUtil(); 
+AlquilerUtil au  = new AlquilerUtil();
+Clienteutil cu = new Clienteutil();
 do
 {
     do
@@ -37,7 +39,6 @@ do
         Console.WriteLine("1. Carro");
         Console.WriteLine("2. Cliente");
         Console.WriteLine("3. Alquiler");
-        Console.WriteLine("4. Elimina: elimina un desayuno");
         Console.WriteLine("0. Salir");
         Console.Write("Seleccione una opción: ");
         string valor = Console.ReadLine();
@@ -66,8 +67,10 @@ do
                     break;
                 case 2:
                     carro_u.Listar();
+                    break;
                 case 3:
                     carro_u.Eliminar();
+                    break ; 
                 case 0:
                     Console.Clear(); 
                     break;
@@ -82,68 +85,82 @@ do
                     }
                 }
 
-            Console.WriteLine("Lista de desayunos creada");
-            Console.WriteLine("Inserte la cantidad de desayunos que desea para su lista:");
-            array_length = int.Parse(Console.ReadLine());
-            Array.Resize(ref desayuno, array_length);
-            Console.WriteLine("Presione cualquier tecla para continuar");
-            Console.ReadKey();
+           
             break;
         case 2:
-            Console.Clear();
-            for (int i = 0; i < desayuno.Length; i++)
+            do
             {
-                Console.WriteLine($"Inserte qué valores quiere almacenar en el {i + 1}° espacio:");
-                desayuno[i] = Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("MENÚ:");
+                Console.WriteLine("1. Crear Cliente");
+                Console.WriteLine("2. Listar Cliente");
+                Console.WriteLine("3. Eliminar Cliente");
+                Console.WriteLine("0. Salir");
+                Console.Write("Seleccione una opción: ");
+                string valor = Console.ReadLine();
+                if (int.TryParse(valor, out menu_number_carro)) ;
+            } while (false);
+            switch (menu_number_carro)
+            {
+                case 1:
+                    Console.Clear();
+                    cu.Crear();
+                    break;
+                case 2:
+                    cu.Listar(); break;
+                case 3:
+                    cu.Eliminar();                break;
+                case 0:
+                    Console.Clear();
+                    break;
+                default:
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Error. Valor no válido");
+                        Console.WriteLine("");
+                        Console.WriteLine("Presione cualquier tecla para continuar");
+                        Console.ReadKey();
+                        break;
+                    }
             }
-            Console.WriteLine("Presione cualquier tecla para continuar");
-            Console.ReadKey();
             break;
         case 3:
-            Console.Clear();
-            Console.WriteLine("Aquí se muestran los valores ingresados con su ubicación:");
-            for (int i = 0; i < desayuno.Length; i++)
+            do
             {
-                Console.WriteLine($"Ubicación: {i} --> Valor: {desayuno[i]}");
-            }
-            Console.WriteLine("Presione cualquier tecla para continuar");
-            Console.ReadKey();
-            break;
-        case 4:
-
-            Console.Clear();
-            Console.WriteLine("¿Qué elemento desea eliminar?");
-            Console.Write("Inserte el nombre del desayuno: ");
-            string nombre_desayuno = Console.ReadLine();
-            if (Array.Exists(desayuno, element => element == nombre_desayuno))
+                Console.Clear();
+                Console.WriteLine("MENÚ:");
+                Console.WriteLine("1. Crear Cliente");
+                Console.WriteLine("2. Listar Cliente");
+                Console.WriteLine("3. Eliminar Cliente");
+                Console.WriteLine("0. Salir");
+                Console.Write("Seleccione una opción: ");
+                string valor = Console.ReadLine();
+                if (int.TryParse(valor, out menu_number_carro)) ;
+            } while (false);
+            switch (menu_number_carro)
             {
-                elim = Array.IndexOf(desayuno, nombre_desayuno);
-                string[] desayuno2 = new string[array_length];//creacion de array 
-                desayuno2 = desayuno;
-                array2_length = array_length - 1;
-                Array.Resize(ref desayuno, array2_length);
-                for (int i = 0, i2 = 0; i2 < desayuno2.Length; i2++)
-                {
-                    if (i2 != elim)
+                case 1:
+                    Console.Clear();                    au.Crear();
+                    break;
+                case 2:
+                    au.Listar(); break;
+                case 3:
+                    au.Eliminar(); break ;
+                case 0:
+                    Console.Clear();
+                    break;
+                default:
                     {
-                        desayuno[i] = desayuno2[i2];
-                        i++;
+                        Console.Clear();
+                        Console.WriteLine("Error. Valor no válido");
+                        Console.WriteLine("");
+                        Console.WriteLine("Presione cualquier tecla para continuar");
+                        Console.ReadKey();
+                        break;
                     }
-                    else { }
-                }
-                        array_length = array2_length;
-                Console.WriteLine($"Se eliminó la posición {elim}, es decir el {elim + 1}° elemento");
-                Console.WriteLine("Presione cualquier tecla para continuar");
-                Console.ReadKey();
-            }
-            else
-            {
-                Console.WriteLine("Elemento no encontrado");
-                Console.WriteLine("");
-                Console.WriteLine("Presione cualquier tecla para continuar");
-                Console.ReadKey();
             }
             break;
+       
         case 0:
             Console.Clear();
             Console.WriteLine("Terminó el programa");
