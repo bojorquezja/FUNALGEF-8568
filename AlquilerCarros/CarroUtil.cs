f570884a293ee44abccfb7eb1d9690e225b48ee6
Wifth <alexander.obando.os2206@gmail.com>
2024-11-28 08:54:11 -0500

    CarroUtil 1.0

--- AlquilerCarros/CarroUtil.cs
+++ AlquilerCarros/CarroUtil.cs
@@ -8,32 +8,48 @@ namespace AlquilerCarros
 {
     public class CarroUtil
     {
-        public static void CrearCarro()
+        public static void CrearCarro(List<Carro> carros)
         {
-            if ( )
+            Console.WriteLine("****** Registro de Carros ******");
+            Console.Write("Ingrese la placa del carro: ");
+            string placa = Console.ReadLine();
+            Console.Write("Ingrese la marca: ");
+            string marca = Console.ReadLine();
+            Console.Write("Ingrese el precio de alquiler: ");
+            double precioalquiler = double.Parse(Console.ReadLine());
+            carros.Add(new Carro { Placa = placa, Marca = marca, PrecioAlquiler = precioalquiler });
+            Console.Clear();
+            Console.WriteLine("Carro registrado correctamente.\n\n");
+        }
+        public static void ListarCarro(List<Carro> carros)
+        {
+            Console.Clear();
+            if (carros.Count == 0)
             {
-
+                Console.WriteLine("No hay carros registrados");
             }
             else
             {
-                Console.Write("Ingrese la placa del carro: ");
-                string placa = Console.ReadLine();
-                Console.Write("Ingrese la marca: ");
-                string marca = Console.ReadLine();
-                Console.Write("Ingrese el precio de alquiler: ");
-                double precioalquiler = double.Parse(Console.ReadLine());
-                carros.Add(new Carro { Placa = placa, Marca = marca, PrecioAlquiler = precioalquiler });
-                Console.Clear();
-                Console.WriteLine("Carro registrado correctamente.\n\n");
+                Console.WriteLine("****** Lista de Carros ******");
+                for (int i = 0; i < carros.Count; i++)
+                {
+                    Console.WriteLine($"{i}: {carros}");
+                }
             }
         }
-        public static void ListarCarro()
+        public static void EliminarCarro(List<Carro> carros)
         {
-
-        }
-        public static void EliminarCarro()
-        {
-
+            Console.Clear();
+            int index = int.Parse(Console.ReadLine());
+            if (index >= 0 && index < carros.Count)
+            {
+                carros.RemoveAt(index);
+                Console.WriteLine("Registro de carro eliminado con éxito.");
+            }
+            else
+            {
+                Console.WriteLine("Indice invalido.");
+            }
         }
     }
 }
